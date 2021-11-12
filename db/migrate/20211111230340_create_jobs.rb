9a1f01cfd3f33e7824dd4476a4deb6343b7ffde8
zaynegoering <zaynegoering@gmail.com>
2021-11-12 14:57:02 -0800

    remove employee id column

--- db/migrate/20211111230340_create_jobs.rb
+++ db/migrate/20211111230340_create_jobs.rb
@@ -4,7 +4,7 @@ class CreateJobs < ActiveRecord::Migration[6.1]
       t.string :title
       t.text :description
       t.integer :employer_id
-      t.integer :employee_id
+      # t.integer :employee_id
 
       t.timestamps
     end
