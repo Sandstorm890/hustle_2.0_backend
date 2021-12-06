03d3f06387a0457ca508658776a506641f168842
zaynegoering <zaynegoering@gmail.com>
2021-12-06 12:56:12 -0800

    modify cors policy

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,13 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#      origins '*'
+#      resource '*', :headers => :any, :methods => [:get, :post, :options]
+#    end
+# end
+
 module HustleBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -36,5 +43,16 @@ module HustleBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    # config.middleware.insert_before 0, "Rack::Cors" do
+    #   allow do
+    #     origins '*'
+    #     resource(
+    #       '*',
+    #       headers: :any,
+    #       methods: [:get, :patch, :put, :delete, :post, :options]
+    #       )
+    #   end
+    # end
   end
 end
+
